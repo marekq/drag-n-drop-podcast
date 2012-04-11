4c6f5f96b4240e29405b858055d30368f79d287d
Marek Kuczynski <marek.kuczynski@gmail.com>
2012-04-11 20:24:06 +0200

    several small fixes

---
+++ cfg.py
@@ -0,0 +1,26 @@
+#!/bin/python
+# define several variables for display in itunes
+
+# the url where your music is accessible
+url = 'http://marek.asia/music'
+
+# a generic name that will be displayed
+name = 'podcast directory'
+
+# the podcast name
+title = 'podcast directory marekq'
+
+# description field
+description = 'private mixtape collection of marek kuczynski'
+
+# your name
+author = 'marek'
+
+# your email
+email = 'marek@marek.asia'
+
+# static content category
+category = 'music'
+
+# music path - if different from current dir, define here
+musicpath = '/var/www/music/'
